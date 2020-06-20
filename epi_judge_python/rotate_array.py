4ddaa1420cab33103cddf84066274a851faf0420
Vinod Singla <vsingla2@ncsu.edu>
2020-06-20 14:20:07 -0400

    feat: implement array rotation

--- epi_judge_python/rotate_array.py
+++ epi_judge_python/rotate_array.py
@@ -6,8 +6,23 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def rotate_array(rotate_amount: int, A: List[int]) -> None:
-    # TODO - you fill in here.
+    # O(n) time, O(1) space
+    if len(A) > 1:
+        n = len(A)
+        rotate_amount %= n  # take care of large numbers
+        if rotate_amount < 0:  # take care of negative numbers
+            rotate_amount += n
+        reverse_array(A, 0, n - 1)
+        reverse_array(A, 0, rotate_amount - 1)
+        reverse_array(A, rotate_amount, n - 1)
     return
+
+
+def reverse_array(array_input, i, j):
+    while i < j:
+        array_input[i], array_input[j] = array_input[j], array_input[i]
+        i += 1
+        j -= 1
 
 
 @enable_executor_hook
