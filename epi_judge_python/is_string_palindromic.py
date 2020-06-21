d88bd4bfd91b45617c962755e934a46573955df6
Vinod Singla <vsingla2@ncsu.edu>
2020-06-21 18:34:42 -0400

    feat: check if string is palindromic

--- epi_judge_python/is_string_palindromic.py
+++ epi_judge_python/is_string_palindromic.py
@@ -1,8 +1,16 @@
 from test_framework import generic_test
 
 
+# O(n) time, O(1) space
 def is_palindromic(s: str) -> bool:
-    # TODO - you fill in here.
+    if len(s) > 1:
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
     return True
 
 
