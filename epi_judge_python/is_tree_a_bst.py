bc26f23e4a970698441c3a5fd7eb397f503b1716
Vinod Singla <vsingla2@ncsu.edu>
2020-06-21 18:33:54 -0400

    feat: check if tree is a binary search tree

--- epi_judge_python/is_tree_a_bst.py
+++ epi_judge_python/is_tree_a_bst.py
@@ -2,9 +2,13 @@ from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
 
-def is_binary_tree_bst(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
-    return True
+def is_binary_tree_bst(tree: BinaryTreeNode, lower_bound=float('-inf'), upper_bound=float('inf')) -> bool:
+    if not tree:
+        return True
+    elif not lower_bound <= tree.data <= upper_bound:  # tree is not null and tree.left is not null
+        return False
+    return (is_binary_tree_bst(tree.left, lower_bound, tree.data) and
+            is_binary_tree_bst(tree.right, tree.data, upper_bound))
 
 
 if __name__ == '__main__':
